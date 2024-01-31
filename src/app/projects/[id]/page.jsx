f6f0c48ee4dcfb64ba147e52693e6f5dba60477b
Himanshi Sharma <himanshish456@gmail.com>
2024-01-31 22:44:19 +0530

    project details content added

--- src/app/projects/[id]/page.jsx
+++ src/app/projects/[id]/page.jsx
@@ -10,28 +10,27 @@ const details = [
     tools: "Reactjs, Vitejs",
     timeline: "Dec 7 - 11, 2023",
     description:
-      "• Built a website which is equipped with 4+ features designed to store, manage and upload books for sale. • The functionalities encompass user authentication through Firebase for login and signup, complemented by a chatbot for engaging user interactions.",
+      "Built a website which is equipped with 4+ features designed to store, manage and upload books for sale. The functionalities encompass user authentication through Firebase for login and signup, complemented by a chatbot for engaging user interactions. Incorporating modules for book inventory, this system facilitates tracking of available books and customer profiles. By utilizing this computerized setup, it mitigates the risk of losing book or customer records, a common occurrence in non-computerized systems.",
   },
   {
     id: 2,
     image: "/book store (1).png",
     colorClass: "bg-green-500",
     title: "Meme Generator",
-    tools: "Reactjs",
-
+    tools: "Reactjs, Rest API",
+    timeline: "April, 2023",
     description:
-      "Discover the alchemy of words as you embark on a journey through‘Express Your Book Love.’ This guide unveils the art of crafting heartfelt essays, inviting you to weave prose that mirrors the depth of your passion for cherished reads",
+      "Developed a web-based Meme Generator allowing users to create custom memes by combining text and images, utilizing an API with access to a diverse library of 100+ images.",
   },
   {
     id: 3,
     image: "/Payment Gateway Integration.png",
     colorClass: "bg-gray-500",
-
     title: "donation",
-    tools: "HTML, CSS",
-
+    tools: "HTML, CSS, Javascript",
+    timeline: "May - June, 2023",
     description:
-      "Discover the alchemy of words as you embark on a journey through‘Express Your Book Love.’ This guide unveils the art of crafting heartfelt essays, inviting you to weave prose that mirrors the depth of your passion for cherished reads",
+      " Developed a web application using with HTML, CSS from scratch as the part of The Sparks Foundation Internship by using Razorpay as a Payment Integration",
   },
   {
     id: 4,
@@ -39,10 +38,11 @@ const details = [
     colorClass: "bg-pink-500",
 
     title: "Travel Journal",
-    tools: "Reactjs",
+    tools: "Reactjs, React Props Learning",
+    timeline: "April, 2023",
 
     description:
-      "Discover the alchemy of words as you embark on a journey through‘Express Your Book Love.’ This guide unveils the art of crafting heartfelt essays, inviting you to weave prose that mirrors the depth of your passion for cherished reads",
+      "It is a static website that I built to learn Reactjs and props",
   },
 ];
 
@@ -50,43 +50,65 @@ function ProjectDetail({ params }) {
   const detail = details.find((d) => d.id == params.id);
   return (
     <div className="flex flex-col justify-between min-h-screen mt-6 mx-10">
-      <div className="flex md:ml-10 mt-3.5 md:mt-0">
-        <Image src={detail.image} alt="" width={500} height={500} />
+      <div className="flex md:ml-10 mt-3.5 md:mt-0 ">
+        <Image
+          src={
+            "https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&w=600"
+          }
+          width={400}
+          height={400}
+        />
+        <Image
+          src={
+            "https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&w=600"
+          }
+          width={400}
+          height={400}
+        />
+        <Image
+          src={
+            "https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&w=600"
+          }
+          width={500}
+          height={400}
+        />
       </div>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row gap-40 mt-10">
         <div className="flex flex-col">
-          <h1 className="text-5xl">Tools</h1>
-          <h2 className="text-3xl">{detail.tools}</h2>
+          <h1 className="text-3xl font-bold">Tools</h1>
+          <h2 className="text-1xl">{detail.tools}</h2>
         </div>
-        <div className="ml-10">
-          <h1 className="text-5xl">Timeline</h1>
-          <h2 className="text-3xl">{detail.timeline}</h2>
+        <div className="ml-20">
+          <h1 className="text-3xl font-bold">Timeline</h1>
+          <h2 className="text-1xl">{detail.timeline}</h2>
+        </div>
+        <div className="mt-3">
+          <Link
+            href="/"
+            className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full mb-2 md:mb-0 md:mr-4"
+          >
+            Github
+          </Link>
+          <Link
+            href="/"
+            className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full ml-10 mb-2 md:mb-0 md:mr-4"
+          >
+            Live Site
+          </Link>
+          <Link
+            href="/"
+            className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full ml-10 mb-2 md:mb-0 md:mr-4"
+          >
+            Demo Video
+          </Link>
         </div>
       </div>
+
       <div>
-        <h1>Brief</h1>
-        <p>{detail.description}</p>
-      </div>
-      <div>
-        <h1>Links</h1>
-        <Link
-          href="/"
-          className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full mb-2 md:mb-0 md:mr-4"
-        >
-          Github
-        </Link>
-        <Link
-          href="/"
-          className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full mb-2 md:mb-0 md:mr-4"
-        >
-          Live Site
-        </Link>
-        <Link
-          href="/"
-          className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full mb-2 md:mb-0 md:mr-4"
-        >
-          Demo Video
-        </Link>
+        <h1 className="text-3xl mt-20 font-bold">Brief</h1>
+        <p className="text-left grid grid-cols-1 gap-6 sm:grid-cols-2 mt-7">
+          {detail.description}
+        </p>
       </div>
       <div>
         <h1>Images</h1>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,12 +7,14 @@ function Navbar() {
   const [nav, setNav] = useState(false);
 
   return (
-    <div className="flex flex-col md:flex-row w-full md:h-[80px] justify-between items-center px-4">
+    <div className=" flex flex-col md:flex-row w-full md:h-[80px] justify-between items-center px-4">
       <div className="font-bold text-7xl font-mono mb-4 md:mb-0">
         <Link href="/">hs</Link>
       </div>
       <div
-        className={`md:flex flex-col md:flex-row ${nav ? "flex" : "hidden"}`}
+        className={`md:flex flex-col md:flex-row ml-auto ${
+          nav ? "flex" : "hidden"
+        }`}
       >
         <Link
           href="/"
@@ -35,7 +37,7 @@ function Navbar() {
       </div>
       <div
         onClick={() => setNav(!nav)}
-        className="md:hidden absolute cursor-pointer right-4 z-10 mt-6"
+        className="md:hidden absolute cursor-pointer right-4 z-10 mt-6 "
       >
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
@@ -44,3 +46,49 @@ function Navbar() {
 }
 
 export default Navbar;
+
+// import Link from "next/link";
+// import React, { useState } from "react";
+// import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+// function Navbar() {
+//   const [nav, setNav] = useState(false);
+
+//   return (
+//     <div className="fixed top-0 left-0 w-full flex flex-col md:flex-row md:h-[80px] justify-between items-center px-4 bg-white shadow-md">
+//       <div className="font-bold text-7xl font-mono mb-4 md:mb-0">
+//         <Link href="/">hs</Link>
+//       </div>
+//       <div
+//         className={`md:flex flex-col md:flex-row ${nav ? "flex" : "hidden"}`}
+//       >
+//         <Link
+//           href="/"
+//           className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full mb-2 md:mb-0 md:mr-4"
+//         >
+//           Work
+//         </Link>
+//         <Link
+//           href="/about"
+//           className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full mb-2 md:mb-0 md:mr-4"
+//         >
+//           About
+//         </Link>
+//         <Link
+//           href="/"
+//           className="bg-white border-2 border-black hover:bg-black hover:text-white hover:border-white py-3 px-8 rounded-full"
+//         >
+//           Resume
+//         </Link>
+//       </div>
+//       <div
+//         onClick={() => setNav(!nav)}
+//         className="md:hidden absolute cursor-pointer right-4 z-10 mt-6"
+//       >
+//         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
